67d9032764763102323cda17be350473668d5c9d
janolis <jan.ruedisuehli@students.fhnw.ch>
2026-05-21 10:45:30 +0200

    second push

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ import streamlit as st
 # Komponenten importieren
 from frontend import render_sidebar, render_main_content
 
-# 1. Seitenkonfiguration (Muss immer die erste Streamlit-Anweisung sein!)
 st.set_page_config(
-    page_title="Stadtplaner Schweiz",
+    page_title="Wohnscore Zürich",
     page_icon="🏙️",
     layout="wide"
 )
 
 # 2. Header der App
-st.title("🏙️ Interaktiver Stadtplaner")
-st.markdown("Finde den perfekten Wohnort in der Schweiz basierend auf deinen persönlichen Gewichtungen.")
+st.title("🏙️ Interaktiver Wohnscore-Planer")
+st.markdown("Finde den perfekten Wohnort in Zürich basierend auf deinen persönlichen Gewichtungen.")
 
 # 3. Sidebar rendern und Eingaben abfangen
 inputs = render_sidebar()
